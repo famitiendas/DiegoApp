64ffbd1d814f823925064613b8cc6978f59a9398
kevin olarte <kolarte@unab.edu.co>
2019-06-07 22:32:55 -0500

    se agrego el menú

--- lib/com/famitiendas/src/widgets/login_screen.dart
+++ lib/com/famitiendas/src/widgets/login_screen.dart
@@ -1,18 +1,137 @@
+import 'package:famitiendas_distribuciones/com/famitiendas/src/widgets/menu.dart';
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
+   static String tag = 'LoginScreen';
+
+  static Route<dynamic> route(){
+return MaterialPageRoute(
+builder: (context)=> LoginScreen(),
+);
+
+  }
+
   LoginScreen({Key key}) : super(key: key);
 
   _LoginScreenState createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+   bool loginEnabled;
+  bool isVisible;
+    BuildContext contexto;
+   TextEditingController _phoneNumber = new TextEditingController();
+  TextEditingController _passw = new TextEditingController();
+final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-       child: new TextField(
+        contexto = context;
+        final number = TextFormField(
+      keyboardType: TextInputType.text,
+      autofocus: false,
+      enabled: loginEnabled != null ? loginEnabled : true,
+      controller: _phoneNumber,
+      decoration: InputDecoration(
+        hintText: 'ingrese usuario',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+      ),
+    );
+    final password = Visibility(
+    //  visible: isVisible != null ? isVisible : true,
+      child: TextFormField(
+        autofocus: false,
+        obscureText: true,
+    //    controller: _passw,
+        validator: (value) {
+          if (value.isEmpty) {
+            return 'ingrese Contraseña';
+          }
+        },
+        decoration: InputDecoration(
+          hintText: 'Contraseña',
+          contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(32.0),
+          ),
+        ),
+      ),
+    );
+    final loginButton = Padding(
+      padding: EdgeInsets.symmetric(vertical: 16.0),
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24),
+        ),
+        onPressed: () {
+       loguearse();
+        },
+        padding: EdgeInsets.all(12),
+        color: Colors.lightBlueAccent,
+        child: Text('Entrar', style: TextStyle(color: Colors.white)),
+      ),
+    );
+     final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/logo.png'),
+      ),
+    );
+    final firstRow = new Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        loginButton,
+        SizedBox(height: 28.0,),
+        Image.asset('assets/logo.png'),
+      ],
+    );
+
+ 
+    return Scaffold(
+      backgroundColor: Colors.white,
+       body:Form(
+           key: _formKey,
+           
+           child: ListView(
+             shrinkWrap: true,
+             padding: EdgeInsets.only(left: 24.0,right: 24.0),
+             children: <Widget>[
+               SizedBox(height: 50.0,),
+               Image.asset('assets/logo.png'),
+               SizedBox(height: 98.0,),
+               new Center(
+                 child: new Column(
+                    children: <Widget>[
+                number,
+               SizedBox(height: 18.0),
+               password,
+                SizedBox(height: 18.0,),
+                loginButton
+                    ],
+                 ),
+               ),
+              
+                SizedBox(height: 98.0,),
+              Image.asset('assets/logo.png')
+
+             ],
+           ),
          
        ),
     );
   }
+
+   void loguearse()async{
+
+    print('Hola mundo');
+    Navigator.of(contexto)
+        .pushReplacement(MaterialPageRoute(builder: (contexto) {
+      return new Menu();
+    }));
+
+ } 
 }
---
+++ lib/com/famitiendas/src/widgets/menu.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+
+class Menu extends StatefulWidget {
+  static String tag = 'Menu';
+
+  static Route<dynamic> route() {
+    return MaterialPageRoute(
+      builder: (context) => Menu(),
+    );
+  }
+
+  Menu({Key key}) : super(key: key);
+
+  _MenuState createState() => _MenuState();
+}
+
+class _MenuState extends State<Menu> {
+  @override
+  Widget build(BuildContext context) {
+    final calificarClientew = Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.0),
+      child: RaisedButton(
+          shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(16.0)),
+          onPressed: () {
+            // loguearse();
+          },
+          padding: EdgeInsets.all(12),
+          color: Colors.lightBlueAccent,
+          child: new Container(
+            height: 80.0,
+            width: 220.0,
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Column(
+                  children: <Widget>[
+                    new Center(
+                      child: new Image.asset(
+                        'assets/man.png',
+                        height: 80.0,
+                        width: 70.0,
+                      ),
+                    ),
+                  ],
+                ),
+                new Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    new Padding(
+                      padding: EdgeInsets.only(left: 14.0, top: 10.0),
+                      child: new Text('Calificar Cliente',
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 18.0)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
+
+    final notificaciones = Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.0),
+      child: RaisedButton(
+          shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(16.0)),
+          onPressed: () {
+            // loguearse();
+          },
+          padding: EdgeInsets.all(12),
+          color: Colors.lightBlueAccent,
+          child: new Container(
+            height: 80.0,
+            width: 220.0,
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Column(
+                  children: <Widget>[
+                    new Center(
+                      child: new Image.asset(
+                        'assets/email.png',
+                        height: 80.0,
+                        width: 70.0,
+                      ),
+                    ),
+                  ],
+                ),
+                new Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    new Padding(
+                      padding: EdgeInsets.only(left: 14.0, top: 10.0),
+                      child: new Text('Notificaciones',
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 18.0)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
+
+    final resultados = Padding(
+      padding: EdgeInsets.symmetric(vertical: 12.0),
+      child: RaisedButton(
+          shape: new RoundedRectangleBorder(
+              borderRadius: new BorderRadius.circular(16.0)),
+          onPressed: () {
+            // loguearse();
+          },
+          padding: EdgeInsets.all(12),
+          color: Colors.lightBlueAccent,
+          child: new Container(
+            height: 80.0,
+            width: 220.0,
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                new Column(
+                  children: <Widget>[
+                    new Center(
+                      child: new Image.asset(
+                        'assets/pie-chart.png',
+                        height: 80.0,
+                        width: 70.0,
+                      ),
+                    ),
+                  ],
+                ),
+                new Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 18.0,
+                    ),
+                    new Padding(
+                      padding: EdgeInsets.only(left: 14.0, top: 10.0),
+                      child: new Text('  Resultados',
+                          style:
+                              TextStyle(color: Colors.white, fontSize: 18.0)),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          )),
+    );
+
+    return Scaffold(
+      body: new Center(
+        child: new Column(
+          children: <Widget>[
+            SizedBox(
+              height: 98.0,
+            ),
+            calificarClientew,
+            notificaciones,
+            resultados
+          ],
+        ),
+      ),
+    );
+  }
+}
