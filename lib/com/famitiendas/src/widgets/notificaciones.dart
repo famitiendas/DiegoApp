6227266279028b9bdac85a7544ed0a6b55005ef1
kevin olarte <kolarte@unab.edu.co>
2019-06-10 21:39:07 -0500

    notificaciones

---
+++ lib/com/famitiendas/src/widgets/notificaciones.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Notificaciones extends StatefulWidget {
+  Notificaciones({Key key}) : super(key: key);
+
+  _NotificacionesState createState() => _NotificacionesState();
+}
+
+class _NotificacionesState extends State<Notificaciones> {
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: new Text('hola feo')
+    );
+  }
+}
